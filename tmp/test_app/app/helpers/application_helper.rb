bbd2bc7c67d7a26faa49111d06a7ec21893559d4
Falk Köppe <falk.koeppe@infopark.de>
2014-04-15 09:29:27 +0200

    reduce application helper

--- tmp/test_app/app/helpers/application_helper.rb
+++ tmp/test_app/app/helpers/application_helper.rb
@@ -4,16 +4,10 @@ module ApplicationHelper
 
     if obj
       attributes[:data] = {
-        current_obj_id: obj.id,
         current_obj_path: obj.path,
-        current_obj_class: obj.obj_class,
       }
     end
 
     attributes
-  end
-
-  def image_url(image)
-    request.protocol + request.host_with_port + image_path(image)
   end
 end
