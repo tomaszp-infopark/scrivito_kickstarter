b2936ef194f9bebd60e2680cebf0d8b5760d36a7
Falk Köppe <falk.koeppe@infopark.de>
2014-04-15 08:36:46 +0200

    simple theme: add welcome teaser widget on homepage

--- lib/generators/cms/theme/simple/templates/migration.rb
+++ lib/generators/cms/theme/simple/templates/migration.rb
@@ -18,11 +18,30 @@ class KickstarterSimpleTheme < ::Scrival::Migration
     # Homepage example
     #
 
+    teaser_widget = Scrival::BasicWidget.new(
+      _obj_class: 'TeaserWidget',
+      headline: 'Welcome to Scrival',
+      content: '<p>You successfully started your
+        project. Basic components such as a top navigation, a search panel, this text widget, and a
+        login page have been created for you to experiment with the building blocks of your website
+        application. To access the documentation or get in touch with the Scrival support team,
+        visit the Scrival Documentation.</p>',
+      link_to: [
+        Scrival::Link.new(
+          url: 'https://scrival.com/preparation',
+          title: 'Browse Scrival Documentation'
+        ),
+      ]
+    )
+
     Obj.create(
       _path: homepage_path,
       _obj_class: 'Homepage',
       locale: 'en',
-      headline: 'Company, Inc.'
+      headline: 'Company, Inc.',
+      main_content: [
+        teaser_widget,
+      ]
     )
 
     #
