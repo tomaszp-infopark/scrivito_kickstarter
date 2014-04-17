db25a20f56cddd7fc5969bcaab75142b8692a564
Falk Köppe <falk.koeppe@infopark.de>
2014-04-17 14:27:40 +0200

    bugfix: wrong attribute name in three column widget

--- tmp/test_app/app/models/three_column_widget.rb
+++ tmp/test_app/app/models/three_column_widget.rb
@@ -1,13 +1,13 @@
 class ThreeColumnWidget < Widget
   def column_1_width
-    self[:column_1_widget].presence || '4'
+    self[:column_1_width].presence || '4'
   end
 
   def column_2_width
-    self[:column_2_widget].presence || '4'
+    self[:column_2_width].presence || '4'
   end
 
   def column_3_width
-    self[:column_3_widget].presence || '4'
+    self[:column_3_width].presence || '4'
   end
 end
